018925b18a739e2baacf0003760ef9e374104594
Greg Gancarz <propterprivatum@tuta.io>
2019-04-24 16:18:14 -0500

    added login and register views

--- controllers/users.js
+++ controllers/users.js
@@ -4,4 +4,15 @@ const Post = require('../models/post')
 const User = require('../models/user')
 const Category = require('../models/category')
 
+router.get('/login', async (req, res) => {
+	res.render('users/login.ejs');
+});
+
+router.get('/register', (req, res) => {
+	res.render('users/new.ejs', {
+
+	});	
+});
+
+
 module.exports = router;
