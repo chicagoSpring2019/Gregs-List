469e418f54dc6c192028f3b4e498484cef553eb0
Matthew Gonczar <mattisagumball@gmail.com>
2019-04-24 23:05:58 -0500

    nav bar cssed

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//node modules
 const express        = require('express');
 const app            = express();
 const bodyParser     = require('body-parser');
@@ -5,30 +6,27 @@ const methodOverride = require('method-override');
 const session        = require('express-session');
 require('./db/db')
 
+//controllers
 const usersController  = require('./controllers/users');
 const postsController = require('./controllers/posts');
 //const categoriesController     = require('./controllers/categoriesController');
 
+//middleware
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
-
 app.use(session({
   secret: 'This is a random secret string that you would make up to protect your session',
   resave: false, 
   saveUninitialized: false
 }));
 
-
-// app.use((req, res, next) => {
-
-// })
-
-
+//controllers
 app.use('/users', usersController);
 app.use('/posts', postsController);
 //app.use('/categories', categoriesController);
 
-
+//listener
 app.listen(3000, () => {
   console.log('listening... on port: ', 3000);
 });
