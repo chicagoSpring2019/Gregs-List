3c0356a83a622e84d7d1c2c878f6f3f7e3f1eec2
Matthew Gonczar <mattisagumball@gmail.com>
2019-04-29 13:12:53 -0500

    restrict creating a user

--- controllers/users.js
+++ controllers/users.js
@@ -28,36 +28,44 @@ router.get('/register', (req, res, next) => {
 
 /// create new user from register page ///
 router.post('/register', async (req, res, next) => {
-    const password = req.body.password;
-    const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-    const userDbEntry = {};
-    userDbEntry.name = req.body.name
-    userDbEntry.password = passwordHash
-    userDbEntry.description = req.body.description
-    userDbEntry.email = req.body.email
-    userDbEntry.phone = req.body.phone
-    userDbEntry.linkedin = req.body.linkedin
-    try {
-        const found = await User.findOne({
-            'name': req.body.name
-        })
-        // if that name is already taken
-        if (found) {
-            req.session.message = "Username already taken."
-            // redirect to register page with a message
-            res.redirect('/users/register')
-        } else {
-            // create user
-            const createdUser = await User.create(userDbEntry)
-            // they will be logged in (session)
-            req.session.loggedIn = true
-            req.session.userId = createdUser._id
-            req.session.name = req.body.name
-            res.redirect('/posts/')
-        }
-    } catch (err) {
-        next(err)
+  const password = req.body.password;
+  const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  const userDbEntry = {};
+  userDbEntry.name = req.body.name
+  userDbEntry.password = passwordHash
+  userDbEntry.description = req.body.description
+  userDbEntry.email = req.body.email
+  userDbEntry.phone = req.body.phone
+  userDbEntry.linkedin = req.body.linkedin
+  try {
+    const foundName = await User.findOne({'name': req.body.name});
+    const foundEmail = await User.findOne({'email': req.body.email});
+    const foundPhone = await User.findOne({'phone': req.body.phone});
+    const foundLinkedin = await User.findOne({'linkedin': req.body.linkedin});
+    if (foundName) {
+      req.session.message = "Username already taken."
+      res.redirect('/users/register')
+    }else if(foundEmail) {
+      req.session.message = "Email already taken."
+      res.redirect('/users/register')
+    }else if(req.body.phone.length > 0 && foundPhone){
+      req.session.message = "Phone Number already taken."
+      res.redirect('/users/register')
+    }else if(req.body.linkedin.length > 0 && foundLinkedin) {
+      req.session.message = "linkedIn already taken."
+      res.redirect('/users/register')
+    } else {
+      // create user
+      const createdUser = await User.create(userDbEntry)
+      // they will be logged in (session)
+      req.session.loggedIn = true
+      req.session.userId = createdUser._id
+      req.session.name = req.body.name
+      res.redirect('/posts/')
     }
+  } catch (err) {
+    next(err)
+  }
 });
 
 // login as a user/ start session
@@ -102,10 +110,13 @@ router.get('/logout', (req, res, next) => {
 router.get('/:id', async (req, res, next) => {
     try {
         const foundUser = await User.findById(req.params.id).populate('posts')
+        const msg = req.session.message
+        req.session.message = ''
         res.render('users/show.ejs', {
             user: foundUser,
             posts: foundUser.posts,
-            session: req.session
+            session: req.session,
+            message: msg
         })
     } catch (err) {
         next(err)
@@ -179,12 +190,9 @@ router.get('/:id/edit', async (req, res, next) => {
     const foundUser = await User.findById(req.params.id);
     if (foundUser._id == req.session.userId) {
         try {
-          const msg = req.session.message
-          req.session.message = ''
           res.render('users/edit.ejs', {
             user: foundUser,
-            session: req.session,
-            msg: message
+            session: req.session
           });
         } catch (err) {
             next(err);
