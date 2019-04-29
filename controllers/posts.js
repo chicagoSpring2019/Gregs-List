2373afc965cadbbf6261088d3e7073ad52dab1b5
Greg Gancarz <propterprivatum@tuta.io>
2019-04-29 11:57:51 -0500

    Post show pages now only display certain fields if those field are not null

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,105 +5,113 @@ const User = require('../models/user')
 
 // MAIN PAGE - Lists all the main posts
 router.get('/', async (req, res, next) => {
-	try{
-		const allPosts = await Post.find({});
-		res.render('posts/index.ejs', {
-			posts: allPosts,
-			session: req.session
-		})
-	}
-	catch(err){
-		next(err)
-	}
+    try {
+        const allPosts = await Post.find({});
+        res.render('posts/index.ejs', {
+            posts: allPosts,
+            session: req.session
+        })
+    } catch (err) {
+        next(err)
+    }
 })
 
 // ROUTE TO THE POST CREATION PAGE
 router.get('/new', (req, res, next) => {
-	// ONLY Reachable if user id logged in
-	if(req.session.loggedIn === true) {
-		res.render('posts/new.ejs', {
-			session: req.session
-		})
-	} else {
-		req.session.message = "must be logged in to contribute";
-		res.redirect('/users/login')	
-	}
+    // ONLY Reachable if user id logged in
+    if (req.session.loggedIn === true) {
+        res.render('posts/new.ejs', {
+            session: req.session
+        })
+    } else {
+        req.session.message = "must be logged in to contribute";
+        res.redirect('/users/login')
+    }
 })
 
 // ROUTE TO POST NEW POSTS
-router.post('/', async (req, res, next) => { 
-	try {
-		const foundUser = await User.findById(req.session.userId);
-		const createdPost = await Post.create(req.body);
-		foundUser.posts.push(createdPost);
-		foundUser.save()
-		res.redirect('/posts')
-	}
-	catch(err) {
-		next(err)
-	}
+router.post('/', async (req, res, next) => {
+    try {
+        const foundUser = await User.findById(req.session.userId);
+        const createdPost = await Post.create(req.body);
+        foundUser.posts.push(createdPost);
+        foundUser.save()
+        res.redirect('/posts')
+    } catch (err) {
+        next(err)
+    }
 })
 
 // SHOW PAGE FOR POSTS
 router.get('/:id', async (req, res, next) => {
-	try{
-		// Returns one post from the specified user, 
-		// that matches the parameters and puts it in an array.
-		const foundUser = await User.findOne({'posts': req.params.id})
-		.populate({path: 'posts', match: {_id: req.params.id}})
-		console.log("\nfoundUser");
-		console.log(foundUser);
-		res.render('posts/show.ejs', {
-			post: foundUser.posts[0],
-			user: foundUser,
-			session: req.session
-		})
-	}
-	catch(err){
-		next(err)
-	}
+    try {
+        // Returns one post from the specified user, 
+        // that matches the parameters and puts it in an array.
+        const foundUser = await User.findOne({
+                'posts': req.params.id
+            })
+            .populate({
+                path: 'posts',
+                match: {
+                    _id: req.params.id
+                }
+            })
+        console.log("\nfoundUser");
+        console.log(foundUser);
+        res.render('posts/show.ejs', {
+            post: foundUser.posts[0],
+            user: foundUser,
+            session: req.session
+        })
+    } catch (err) {
+        next(err)
+    }
 })
 
 // ROUTE to the post edit page
 router.get('/:id/edit', async (req, res, next) => {
-	const foundUser = await User.findOne({'posts': req.params.id})
-	if(foundUser._id == req.session.userId){
-		try{
-			const foundPost = await Post.findOne({_id: req.params.id})
-			res.render('posts/edit.ejs', {
-				post: foundPost,
-				session: req.session
-			})
-		}
-		catch(err){
-			next(err)
-		}
-	}
-	else{
-		res.redirect('/posts/' + req.params.id)
-	}
+    const foundUser = await User.findOne({
+        'posts': req.params.id
+    })
+    if (foundUser._id == req.session.userId) {
+        try {
+            const foundPost = await Post.findOne({
+                _id: req.params.id
+            })
+            res.render('posts/edit.ejs', {
+                post: foundPost,
+                session: req.session
+            })
+        } catch (err) {
+            next(err)
+        }
+    } else {
+        res.redirect('/posts/' + req.params.id)
+    }
 })
 
 // ROUTE for updating posts
 router.put('/:id', async (req, res, next) => {
-	try{
-		const updatePost = await Post.findByIdAndUpdate(req.params.id, req.body, {new: true});
-		res.redirect('/posts/' + req.params.id)
-	}
-	catch(err){
-		next(err)
-	}
+    try {
+        const updatePost = await Post.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
+        });
+        res.redirect('/posts/' + req.params.id)
+    } catch (err) {
+        next(err)
+    }
 })
 
 // DELETE ROUTE for posts
 router.delete('/:id', async (req, res, next) => {
-	try{
-		const deletePost = await Post.deleteOne({_id: req.params.id})
-		res.redirect('/posts')
-	}
-	catch(err){
-		next(err)
-	}
+    try {
+        const deletePost = await Post.deleteOne({
+            _id: req.params.id
+        })
+        res.redirect('/posts')
+    } catch (err) {
+        next(err)
+    }
 })
 
 module.exports = router;
--- controllers/users.js
+++ controllers/users.js
@@ -7,168 +7,170 @@ const bcrypt = require('bcryptjs')
 ///////////////// AUTH //////////////
 //show login page
 router.get('/login', async (req, res, next) => {
-  const msg = req.session.message
-  req.session.message = ''
-	res.render('users/login.ejs', {
-    message: msg,
-    session: req.session
-  });
+    const msg = req.session.message
+    req.session.message = ''
+    res.render('users/login.ejs', {
+        message: msg,
+        session: req.session
+    });
 });
 
 //show register page
 router.get('/register', (req, res, next) => {
-	const msg = req.session.message
-	req.session.message = ''
-	res.render('users/new.ejs', {
-    message: msg,
-    session: req.session
-	});	
+    const msg = req.session.message
+    req.session.message = ''
+    res.render('users/new.ejs', {
+        message: msg,
+        session: req.session
+    });
 });
 
 /// create new user from register page ///
 router.post('/register', async (req, res, next) => {
-  const password = req.body.password;
-  const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-  const userDbEntry = {};
+    const password = req.body.password;
+    const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+    const userDbEntry = {};
     userDbEntry.name = req.body.name
     userDbEntry.password = passwordHash
     userDbEntry.description = req.body.description
     userDbEntry.email = req.body.email
     userDbEntry.phone = req.body.phone
-    userDbEntry.linkedin = req.body.linkedin 
-  try {
-    const found = await User.findOne({'name': req.body.name})
-    // if that name is already taken
-    if(found) {
-      req.session.message = "Username already taken."
-      // redirect to register page with a message
-      res.redirect('/users/register')
-    }
-    else {
-      // create user
-      const createdUser = await User.create(userDbEntry)
-      // they will be logged in (session)
-      req.session.loggedIn = true 
-      req.session.userId = createdUser._id
-      req.session.name = req.body.name
-      res.redirect('/posts/')
-    }
-  } catch(err){
-      next(err)
+    userDbEntry.linkedin = req.body.linkedin
+    try {
+        const found = await User.findOne({
+            'name': req.body.name
+        })
+        // if that name is already taken
+        if (found) {
+            req.session.message = "Username already taken."
+            // redirect to register page with a message
+            res.redirect('/users/register')
+        } else {
+            // create user
+            const createdUser = await User.create(userDbEntry)
+            // they will be logged in (session)
+            req.session.loggedIn = true
+            req.session.userId = createdUser._id
+            req.session.name = req.body.name
+            res.redirect('/posts/')
+        }
+    } catch (err) {
+        next(err)
     }
 });
 
 // login as a user/ start session
 router.post('/login', async (req, res, next) => {
-  try {
-    const foundUser = await User.findOne({'name': req.body.name});
-    if(foundUser){
-      if(bcrypt.compareSync(req.body.password, foundUser.password) === true){
-        req.session.message = '';
-        req.session.loggedIn = true;
-        req.session.userId = foundUser._id;
-        req.session.name = req.body.name
-        res.redirect('/posts');
-      } else {
-        req.session.message = "Username or Password is incorrect";
-        res.redirect('/users/login');
-      }
-    } else {
-      req.session.message = 'Username or Password is incorrect';
-      res.redirect('/users/login');
+    try {
+        const foundUser = await User.findOne({
+            'name': req.body.name
+        });
+        if (foundUser) {
+            if (bcrypt.compareSync(req.body.password, foundUser.password) === true) {
+                req.session.message = '';
+                req.session.loggedIn = true;
+                req.session.userId = foundUser._id;
+                req.session.name = req.body.name
+                res.redirect('/posts');
+            } else {
+                req.session.message = "Username or Password is incorrect";
+                res.redirect('/users/login');
+            }
+        } else {
+            req.session.message = 'Username or Password is incorrect';
+            res.redirect('/users/login');
+        }
+    } catch (err) {
+        next(err);
     }
-  } catch(err){
-    next(err);
-  }
 });
 
 // logout as a user/ end session
 router.get('/logout', (req, res, next) => {
-  req.session.destroy((err) => {
-    if(err){
-      res.send(err);
-    } else {
-      res.redirect('/users/login');
-    }
-  })
+    req.session.destroy((err) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.redirect('/users/login');
+        }
+    })
 })
 ////////// ^^^^ AUTH ^^^ ///////////////
 
 // USER profile show 
 router.get('/:id', async (req, res, next) => {
-  try{
-  	const foundUser = await User.findById(req.params.id).populate('posts')
-  	res.render('users/show.ejs',{
-  		user: foundUser,
-  		posts: foundUser.posts,
-      session: req.session
-  	})	
-  } 
-  catch(err){
-  	next(err)
-  }
+    try {
+        const foundUser = await User.findById(req.params.id).populate('posts')
+        res.render('users/show.ejs', {
+            user: foundUser,
+            posts: foundUser.posts,
+            session: req.session
+        })
+    } catch (err) {
+        next(err)
+    }
 })
 
 //deletes user and all posts
 router.delete('/:id', async (req, res, next) => {
-  try{
-    const deletedUser = await User.findByIdAndDelete(req.params.id);
-    const deletedUsersPosts = await Post.deleteMany({_id: {$in: deletedUser.posts}});
-    req.session.destroy();
-    res.redirect('/posts')
-  }  
-  catch(err) {
-    next(err);
-  }
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+        const deletedUsersPosts = await Post.deleteMany({
+            _id: {
+                $in: deletedUser.posts
+            }
+        });
+        req.session.destroy();
+        res.redirect('/posts')
+    } catch (err) {
+        next(err);
+    }
 })
 
 //shows user edit page
 router.get('/:id/edit', async (req, res, next) => {
-  const foundUser = await User.findById(req.params.id);
-  if(foundUser._id == req.session.userId){
-    try {
-      res.render('users/edit.ejs', {
-        user: foundUser,
-        session: req.session
-      });
+    const foundUser = await User.findById(req.params.id);
+    if (foundUser._id == req.session.userId) {
+        try {
+            res.render('users/edit.ejs', {
+                user: foundUser,
+                session: req.session
+            });
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        res.redirect('/users/' + req.params.id)
     }
-    catch(err) {
-      next(err);
-    }
-  }
-  else{
-    res.redirect('/users/' + req.params.id)
-  }
 });
 
 //shows profile delete page
 router.get('/:id/delete', async (req, res, next) => {
-  const foundUser = await User.findById(req.params.id);
-  if(foundUser._id == req.session.userId){
-    try {
-      res.render('users/delete.ejs', {
-        user: foundUser,
-        session: req.session
-      });
+    const foundUser = await User.findById(req.params.id);
+    if (foundUser._id == req.session.userId) {
+        try {
+            res.render('users/delete.ejs', {
+                user: foundUser,
+                session: req.session
+            });
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        res.redirect('/users/' + req.params.id)
     }
-    catch(err) {
-      next(err);
-    }
-  }
-  else{
-    res.redirect('/users/' + req.params.id)
-  }
 });
 
 // update the user
 router.put('/:id', async (req, res, next) => {
-  try {
-    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {new: true});
-    res.redirect('/users/' + req.params.id)
-  }  
-  catch(err) {
-    next(err)
-  }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
+        });
+        res.redirect('/users/' + req.params.id)
+    } catch (err) {
+        next(err)
+    }
 })
 
 module.exports = router;
